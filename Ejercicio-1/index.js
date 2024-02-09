67f311cb03b7f2912827446a8897ab381a9a337f
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-02-09 17:50:14 +0100

    Ejercicio-1

---
+++ Ejercicio-1/index.js
@@ -0,0 +1,31 @@
+// 1.1 Usa querySelector para mostrar
+//por consola el botón con la clase .showme
+const btn = document.querySelector(".showme");
+console.log("btn");
+
+// 1.2 Usa querySelector para mostrar
+// por consola el h1 con el id #pillado
+const h1 = document.querySelector("#pillado");
+console.log(h1);
+
+// 1.3 Usa querySelector para mostrar
+// por consola todos los p
+const allP = document.querySelectorAll("p");
+console.log(allP);
+
+// 1.4 Usa querySelector para mostrar
+// por consola todos los elementos con la clase.pokemon
+const pokemon = document.querySelectorAll(".pokemon");
+console.log(pokemon);
+
+// 1.5 Usa querySelector para mostrar
+// por consola todos los elementos con el atributo data-function="testMe".
+const dataFunction = document.querySelectorAll("[data-function]");
+console.log(dataFunction);
+
+// 1.6 Usa querySelector para mostrar
+// por consola el 3 personaje con el atributo
+// data-function="testMe".
+
+// const dataFunction2 = document.querySelectorAll("[data-function][2]");
+// console.log(dataFunction2);
